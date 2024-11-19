5694f4343e85d11a27bfccbb14c1731862086ab6
KurenaiOuji <126101022+KurenaiOuji@users.noreply.github.com>
2024-11-19 16:08:45 -0600

    Logo
    Puse el logo en el proyecto

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import logo from './img/celare.jpg'
 
 export const Logo = () => {
 	return (
@@ -6,14 +7,25 @@ export const Logo = () => {
 			to='/'
 			className={`text-2xl font-bold tracking-tighter transition-all`}
 		>
-			<p className='hidden lg:block'>
-				Celulares
-				<span className='text-cyan-600'>Baratos</span>
-			</p>
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+    <img 
+        src={logo} 
+        alt="Image" 
+        style={{ 
+            width: '64px', 
+            height: 'auto', 
+            borderRadius: '50%', 
+            objectFit: 'cover', 
+            marginRight: '10px' // Space between image and text
+        }} 
+    />
+    <span>Celare</span>
+</div>
+			
 
 			<p className='flex text-4xl lg:hidden'>
 				<span className='-skew-x-6'>C</span>
-				<span className='text-cyan-600 skew-x-6'>B</span>
+				<span className='text-cyan-600 skew-x-6'>L</span>
 			</p>
 		</Link>
 	);
